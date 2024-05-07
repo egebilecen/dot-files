2a67ed6ceda731c2c455b51bc86b8795a3593793
Ege Bilecen <ege@r0m.cc>
2024-05-07 21:23:03 +0100

    nvim - keybind code position re-arrangements

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -1,12 +1,3 @@
--- DAP
-local dap = require("dap")
-
-vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
-vim.keymap.set("n", "<F5>", dap.continue, {})
-vim.keymap.set("n", "<F10>", dap.step_over, {})
-vim.keymap.set("n", "<F11>", dap.step_into, {})
-vim.keymap.set("n", "<F12>", dap.step_out, {})
-
 -- LSP
 vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
@@ -17,6 +8,7 @@ vim.keymap.set("n", "<leader>f", vim.diagnostic.open_float, {})
 vim.keymap.set("n", "<leader>F", vim.diagnostic.setqflist, {})
 vim.keymap.set("n", "[f", vim.diagnostic.goto_prev, {})
 vim.keymap.set("n", "]f", vim.diagnostic.goto_next, {})
+vim.keymap.set("n", "ff", vim.lsp.buf.format, {})
 
 if vim.lsp.inlay_hint then
 	vim.keymap.set("n", "<leader>ih", function()
@@ -28,9 +20,6 @@ end
 vim.keymap.set("n", "<leader>b", ":Neotree filesystem reveal left<CR>")
 vim.keymap.set("n", "<leader>B", ":Neotree close<CR>")
 
--- Language Server
-vim.keymap.set("n", "ff", vim.lsp.buf.format, {})
-
 -- Fuzzy Finder
 local telescope = {
 	builtin = require("telescope.builtin"),
@@ -39,8 +28,17 @@ local telescope = {
 vim.keymap.set("n", "<leader>p", telescope.builtin.find_files, {})
 
 ---- Need to install "ripgrep" packet for it to work. E.g. `apt install ripgrep`.
-vim.keymap.set("n", "<leader>g", telescope.builtin.live_grep, {})
+vim.keymap.set("n", "<leader>gr", telescope.builtin.live_grep, {})
 
 -- Git Signs
 vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
 vim.keymap.set("n", "<leader>gg", ":Git ", {})
+
+-- DAP
+local dap = require("dap")
+
+vim.keymap.set("n", "<leader>dt", dap.toggle_breakpoint, {})
+vim.keymap.set("n", "<F5>", dap.continue, {})
+vim.keymap.set("n", "<F10>", dap.step_over, {})
+vim.keymap.set("n", "<F11>", dap.step_into, {})
+vim.keymap.set("n", "<F12>", dap.step_out, {})
