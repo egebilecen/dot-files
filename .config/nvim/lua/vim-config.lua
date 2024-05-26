f91eb60b7d84d54e25c2062116e28a2ef5016589
Ege Bilecen <ege@r0m.cc>
2024-05-26 22:08:09 +0100

    nvim - enabled current cursor line highlighting

--- .config/nvim/lua/vim-config.lua
+++ .config/nvim/lua/vim-config.lua
@@ -3,6 +3,7 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set relativenumber")
+vim.cmd("set cursorline")
 
 -- vim.cmd("nmap <C-h> <C-w>h")
 -- vim.cmd("nmap <C-j> <C-w>j")
