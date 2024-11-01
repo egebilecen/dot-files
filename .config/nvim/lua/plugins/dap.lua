20a095ec743052b23e3f108179e88f1f3f64a795
Ege Bilecen <ege@r0m.cc>
2024-11-01 17:05:11 +0000

    nvim - added mason-dap and removed DAP cpp config

--- .config/nvim/lua/plugins/dap.lua
+++ .config/nvim/lua/plugins/dap.lua
@@ -15,37 +15,37 @@ return {
 
 		dapui.setup()
 
-		dap.adapters.cppdbg = {
-			id = "cppdbg",
-			type = "executable",
-			-- Path to the debug adapter.
-			command = "/root/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
-		}
+		-- dap.adapters.cppdbg = {
+		-- 	id = "cppdbg",
+		-- 	type = "executable",
+		-- 	-- Path to the debug adapter.
+		-- 	command = "/root/vscode-cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+		-- }
 
-		dap.configurations.cpp = {
-			{
-				name = "Launch file",
-				type = "cppdbg",
-				request = "launch",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-				cwd = "${workspaceFolder}",
-				stopAtEntry = false,
-			},
-			{
-				name = "Attach to gdbserver :1234",
-				type = "cppdbg",
-				request = "launch",
-				MIMode = "gdb",
-				miDebuggerServerAddress = "localhost:1234",
-				miDebuggerPath = "/usr/bin/gdb",
-				cwd = "${workspaceFolder}",
-				program = function()
-					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
-				end,
-			},
-		}
+		-- dap.configurations.cpp = {
+		-- 	{
+		-- 		name = "Launch file",
+		-- 		type = "cppdbg",
+		-- 		request = "launch",
+		-- 		program = function()
+		-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		-- 		end,
+		-- 		cwd = "${workspaceFolder}",
+		-- 		stopAtEntry = false,
+		-- 	},
+		-- 	{
+		-- 		name = "Attach to gdbserver :1234",
+		-- 		type = "cppdbg",
+		-- 		request = "launch",
+		-- 		MIMode = "gdb",
+		-- 		miDebuggerServerAddress = "localhost:1234",
+		-- 		miDebuggerPath = "/usr/bin/gdb",
+		-- 		cwd = "${workspaceFolder}",
+		-- 		program = function()
+		-- 			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+		-- 		end,
+		-- 	},
+		-- }
 
 		dap.listeners.before.attach.dapui_config = function()
 			KEYS("<leader>B")
@@ -60,11 +60,13 @@ return {
 		dap.listeners.before.event_terminated.dapui_config = function()
 			dapui.close()
 			KEYS("<leader>b")
+			KEYS("<A-l>")
 		end
 
 		dap.listeners.before.event_exited.dapui_config = function()
 			dapui.close()
 			KEYS("<leader>b")
+			KEYS("<A-l>")
 		end
 	end,
 }
