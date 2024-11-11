deb672a630ae954b9934c82ef54a393bdce57054
Ege Bilecen <ege@r0m.cc>
2024-11-11 19:58:13 +0000

    nvim - updated dashboard layout and colors

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -8,9 +8,12 @@ return {
 				packages = { enable = false },
 				header = require("ascii").art.misc.hydra.hydra,
 				shortcut = {
-					{ desc = "󰊳 Update", group = "@property", action = "Lazy update", key = "u" },
-					{ desc = " Mason", group = "@property", action = "Mason", key = "m" },
+					{ desc = "󰊳 Update", group = "@string", action = "Lazy update", key = "u" },
+					{ desc = " Mason", group = "@comment.todo", action = "Mason", key = "m" },
+					{ desc = " Projects", group = "@module", action = "SessionSearch", key = "p" },
 				},
+				project = { enable = false },
+				mru = { enable = false },
 				footer = function()
 					local stats = require("lazy").stats()
 					local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
@@ -22,6 +25,9 @@ return {
 				end,
 			},
 		})
+
+		vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#9cdcfe" })
+		vim.api.nvim_set_hl(0, "DashboardFooter", { fg = "#808080" })
 	end,
 	dependencies = {
 		{ "nvim-tree/nvim-web-devicons" },
